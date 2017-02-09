e5ec7496ea00d149ade1e15df75c7f25cb65997d
masterex1000 <masterex1000@users.noreply.github.com>
2017-02-08 21:24:15 -0700

    add clear command

--- server.js
+++ server.js
@@ -1,30 +1,35 @@
 var http = require('http'),
     url = require('url');
-var tweets = {"tweets":[]};
+var tweets = {
+    "tweets": []
+};
 
-http.createServer(function (req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-    query = url.parse(req.url,true).query;
+http.createServer(function(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    query = url.parse(req.url, true).query;
     var response = {};
     //res.end(JSON.stringify(query));
-    if(query.action){
-      console.log("Moving on to phase two!");
-      if(query.action === "read") {
-          console.log("Listing off the posts");
-          response = tweets;
-      }
-      else {
-        if(query.action === "post"){
-          if(query.message) {
-            var tweet = {};
-            tweet.message = query.message;
-            tweets.tweets.push(tweet);
+    if (query.action) {
+        console.log("Moving on to phase two!");
+        if (query.action === "read") {
+            console.log("Listing off the posts");
+            response = tweets;
+        } else if (query.action === "post") {
+            if (query.message) {
+                var tweet = {};
+                tweet.message = query.message;
+                tweets.tweets.push(tweet);
+                response.stat = "All Good!";
+            }
+        } else if (query.action === "clear") {
+            tweets = {
+                "tweets": []
+            };
             response.stat = "All Good!";
-          }
         }
-      }
+
     } else {
-      response.error = "No Action";
+        response.error = "No Action";
     }
     response.url = req.url;
     res.write(JSON.stringify(response));
